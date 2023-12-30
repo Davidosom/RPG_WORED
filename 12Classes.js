785bf8dcd7d3fb3af5b8ea0c8a18960d227ad579
Davidosom <135185879+Davidosom@users.noreply.github.com>
2023-12-30 15:23:17 -0300

    Add files via upload

---
+++ 12Classes.js
@@ -0,0 +1,28 @@
+document.getElementById('atirador').onclick = function() {
+    var mensagem = document.getElementById('mostrarA');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+  };
+  document.getElementById('barbaro').onclick = function() {
+    var mensagem = document.getElementById('mostrarB');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+  };
+  document.getElementById('espadachim').onclick = function() {
+    var mensagem = document.getElementById('mostrarE');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+  };
+  document.getElementById('lutador').onclick = function() {
+    var mensagem = document.getElementById('mostrarLU');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+  };
+  document.getElementById('ladrao').onclick = function() {
+    var mensagem = document.getElementById('mostrarLA');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+  };
+  document.getElementById('musicista').onclick = function() {
+    var mensagem = document.getElementById('mostrarM');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+  };
+  document.getElementById('xama').onclick = function() {
+    var mensagem = document.getElementById('mostrarX');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+  };
