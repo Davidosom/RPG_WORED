9bf30341aa8609f3a59646b6ecb6948ceff05a79
Davidosom <135185879+Davidosom@users.noreply.github.com>
2023-12-31 13:09:28 -0300

    Add files via upload

---
+++ 15Racas.js
@@ -0,0 +1,44 @@
+document.getElementById('Anjo').onclick = function () {
+    var mensagem = document.getElementById('anjo');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Aquático').onclick = function () {
+    var mensagem = document.getElementById('aquático');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Bestial').onclick = function () {
+    var mensagem = document.getElementById('bestial');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Druida').onclick = function () {
+    var mensagem = document.getElementById('druida');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Demônio').onclick = function () {
+    var mensagem = document.getElementById('demônio');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Draconato').onclick = function () {
+    var mensagem = document.getElementById('draconato');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Elementais').onclick = function () {
+    var mensagem = document.getElementById('elementais');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Fada').onclick = function () {
+    var mensagem = document.getElementById('fada');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Humano').onclick = function () {
+    var mensagem = document.getElementById('humano');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Vampiro').onclick = function () {
+    var mensagem = document.getElementById('vampiro');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
+document.getElementById('Yamon').onclick = function () {
+    var mensagem = document.getElementById('yamon');
+    mensagem.style.display = (mensagem.style.display == 'none' ? 'block' : 'none');
+};
